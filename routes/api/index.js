3a397f3c28ef040b1ddf8b3f2cf88e7173f6ab60
Tracy Derraco <ecert31@gmail.com>
2022-09-07 21:48:42 -0700

    get routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,10 @@ const categoryRoutes = require("./category-routes");
 const productRoutes = require("./product-routes");
 const tagRoutes = require("./tag-routes");
 //router.use('/api', apiRoutes);
-router.use("/categories", categoryRoutes);
-router.use("/products", productRoutes);
-router.use("/tags", tagRoutes);
+
+router.use("/api/categories", categoryRoutes);
+router.use("/api/products", productRoutes);
+router.use("/api/tags", tagRoutes);
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
 });
